9f6da436c17a1d4d088d8065eeb3db88a390d8b4
sophiadejong <sophiamarinadejongy@gmail.com>
2018-03-13 10:50:38 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -137,6 +137,7 @@ var captions = {
 	'47': '"Horizonte Vertical" mixed media poster, 2017',
 	'48': '"Ampersand" poster, 2017',
 	'49': 'Unmasked London, Founded in 2015 by avid and visionary minds, London based techno organisation: Unmasked, is born. Bringing together and uniting as one the people with the music. A journey into a nocturnal space, which glides through the confines between electronic, industrial techno music. Showcased through the transcendent sound system of Funktion One and a live visual sense, each without compromise. Proposing rising artists, selected for their quality of sound and performance. Delivering DJ sets synergising cdj with vinyl, live hybrid performances and authenticity behind the console. Unmasked poster April edition, London, UK, 2017',
+	'52': '"Υποβρύχια", 2018',	
 	'222': '',
 	'223': 'sophiamarinadejongy@gmail.com +31614906568',	
 	'3333': 'soundcloud.com/neithme',
