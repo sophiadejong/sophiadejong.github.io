f2bb6960b98b10dc4dcd0320c29128772a5f2d2f
sophiadejong <sophiamarinadejongy@gmail.com>
2018-04-09 12:48:08 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -140,7 +140,7 @@ var captions = {
 	'48': '"Ampersand" poster, 2017',
 	'49': 'Unmasked London, Founded in 2015 by avid and visionary minds, London based techno organisation: Unmasked, is born. Bringing together and uniting as one the people with the music. A journey into a nocturnal space, which glides through the confines between electronic, industrial techno music. Showcased through the transcendent sound system of Funktion One and a live visual sense, each without compromise. Proposing rising artists, selected for their quality of sound and performance. Delivering DJ sets synergising cdj with vinyl, live hybrid performances and authenticity behind the console. Unmasked poster April edition, London, UK, 2017',
 	'52': '"Υποβρύχια", 2018',	
-	'60': 'Deviant 4th Anniversary witg Vladw, Zedje & Arseny [Live], Unfinished Portraits [Live] and Ceremonial Use of Light [Live] at Cocoliche, Buenos Aires, Argentina, 2018',
+	'60': 'Deviant 4th Anniversary with Vladw, Zedje & Arseny [Live], Unfinished Portraits [Live] and Ceremonial Use of Light [Live] at Cocoliche, Buenos Aires, Argentina, 2018',
 	'222': '',
 	'223': 'sophiamarinadejongy@gmail.com +31614906568',	
 	'3333': 'soundcloud.com/neithme',
